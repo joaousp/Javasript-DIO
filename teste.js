fbce1ecd58e91a4db0d3cccad7ac965118c26be7
João Paulo Garcia Martinelli de Oliveira <joao.martinelli@outlook.com.br>
2022-07-19 18:11:45 -0300

    primeira funcao

--- teste.js
+++ teste.js
@@ -1,1 +1,14 @@
-console.log("Hello world");
+function returnEvenValues(array){
+    let evenNums=[];
+    for(let i = 0; i<array.length; i++){
+        if(array[i]%2==0) {
+            evenNums.push(array[i]);
+        }
+    }
+    console.log(evenNums);
+}
+
+
+let array = [1,2,3,4];
+
+returnEvenValues(array);
