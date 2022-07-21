da746591f51240372bff53d1fe8b04717eceae11
João Paulo Garcia Martinelli de Oliveira <joao.martinelli@outlook.com.br>
2022-07-21 20:26:13 -0300

    MFR

---
+++ Map-Filter-Reduce/Playground.js
@@ -0,0 +1,59 @@
+let num = [1,2,3,4,5];
+
+let maca={
+    preco:2
+}
+
+let banana={
+    preco:3
+}
+
+function mapArray(){
+    let num = [1,2,3,4,5];
+
+    return num.map(function(item){
+        return item*this.preco;
+    },banana)
+}
+
+function mapArrayT(cid){
+
+    let num = [1,2,3,4,5];
+    return num.map(function(item){
+        return item*cid.preco;
+    })
+
+}
+
+let numeros = [1,2,3,4,5];
+
+function filterArray(arr){
+    if(!arr||!arr.legth) return;
+
+    const ret = arr.filter((item) => item%2===0);
+    return ret;
+     
+}
+
+
+function reduceArray(){
+   
+
+    const ret = ((curr,accumulator) => curr+accumulator);
+    return ret;
+     
+}
+
+
+function reduceArrayT(numeros,disponivel){
+   
+
+    const ret = numeros.reduce((curr,item)=> curr-item,disponivel);
+    
+    return (ret);
+     
+}
+
+saldo=30;
+console.log(reduceArrayT(numeros,saldo))
+
